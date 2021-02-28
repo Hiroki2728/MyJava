6f5514e4aa1cd640115180b308cda325d934290e
Hiroki2728 <moxx.jh@gmail.com>
2021-02-28 19:10:52 +0900

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,9 +1,11 @@
 class Greeting {
   public static void main(String[] args) {
     int x = 8;
-    int y = 3;
+    int y = 8;
+    x++;
+    y--;
 
-    System.out.println(x >=5 && x <= 10);
-    System.out.println(y >=5 && y <= 10);
+    System.out.println(x);
+    System.out.println(y);
   }
 }
