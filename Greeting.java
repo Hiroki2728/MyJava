9c923cac873ebd661d9d3f160e6f243bf4f12ba0
Hiroki2728 <moxx.jh@gmail.com>
2021-03-01 23:17:27 +0900

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,10 +1,11 @@
 class Greeting {
   public static void main(String[] args) {
     int arr[] = {2,4,6,8,10};
+    int sum = 0;
 
     for( int i = 0; i <= 4; i++){
-      System.out.println(arr[i]);
-    }
-    
+      sum += arr[i];
+      }
+      System.out.println(sum);
   }
 }
