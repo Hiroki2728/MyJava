7b3c95c3c6708239060d6dbba71ddc4ed07eaff0
Hiroki2728 <moxx.jh@gmail.com>
2021-03-01 22:49:21 +0900

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,13 +1,7 @@
 class Greeting {
   public static void main(String[] args) {
-    int age = 0;
-
-    if (age >= 20){
-      System.out.println("adult");
-    } else if (age==0){
-      System.out.println("baby");
-    } else {
-      System.out.println("child");
+    for(int i = 0; i <= 4; i++){
+      System.out.println(i);
     }
   }
 }
