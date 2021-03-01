38b22a0b4b477d789bbb2f86aabc0b9fe7f081c0
Hiroki2728 <moxx.jh@gmail.com>
2021-03-01 23:07:30 +0900

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,10 +1,9 @@
 class Greeting {
   public static void main(String[] args) {
-    for(int i = 0; i <= 4; i ++){
-      if(i==3){
-        continue;
+    for(int i = 0; i <= 2; i ++){
+      for(int j = 0; j <= 2; j ++){
+        System.out.println( i + "-" + j);
       }
-      System.out.println(i);
     }
   }
 }
