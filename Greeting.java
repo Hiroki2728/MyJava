58240ed3a99bd54306ea7ea2ae61df3127cf5bd9
Hiroki2728 <moxx.jh@gmail.com>
2021-03-02 23:53:11 +0900

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,11 +1,17 @@
-class Greeting {
-  public static void main(String[] args) {
-    int arr[] = {2,4,6,8,10};
-    int sum = 0;
+class Student{
+  String name = "sato";
 
-    for( int i = 0; i <= 4; i++){
-      sum += arr[i];
-      }
-      System.out.println(sum);
+  public void calculateAvg(int math, int english){
+  System.out.println((math + english)/2);
+  }
+}
+
+public class Greeting {
+  public static void main(String[] args) {
+  Student a001 = new Student();
+  a001.name = "sato";
+
+  System.out.println(a001.name);
+  a001.calculatedAvg(90,80);
   }
 }
