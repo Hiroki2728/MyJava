5dfada8bcd1ee89cce149ecbdcd492e55003519a
Hiroki2728 <moxx.jh@gmail.com>
2021-03-04 23:19:48 +0900

    Update Lessen15.java

--- Lessen15.java
+++ Lessen15.java
@@ -1,3 +1,26 @@
+class Student{
+  String name;
+
+  public double calculateAvg(double[] data){
+    double sum = 0;
+    for( int i=0; i < data.length; i++){
+            sum += data[i];
+          }
+    double avg = sum/data.length;
+    return avg;
+  }
+
+  public String judge(double avg){
+    String result;
+    if( avg >= 60){
+      result = "passed";
+    } else {
+      result = "failed";
+    }
+    return result;
+  }
+}
+
 public class Lessen15 {
   public static void main(String[] args){
     Student a001 = new Student();
