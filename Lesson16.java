d4d4ad05c55eea602ad472438071ddb03f758d23
Hiroki2728 <moxx.jh@gmail.com>
2021-03-06 23:48:28 +0900

    Update Lesson16.java

--- Lesson16.java
+++ Lesson16.java
@@ -21,18 +21,3 @@ class Student{
   }
 }
 
-
-public class Lesson16 {
-  public static void main(String[] args){
-    Student a001 = new Student();
-    a001.name = "sato";
-    double[] data = {70,65,50,90,30};
-
-    double avg = a001.calculateAvg(data);
-    String result = a001.judge(avg);
-
-
-    System.out.println(avg);
-    System.out.println(a001.name + "" + result);
-
-}
